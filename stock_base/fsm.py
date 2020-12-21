5279a5bc0bbfcd81973b6737c68f721024dedf8a
unknown <jason871110@gmail.com>
2020-12-21 18:19:06 +0800

    add back button

--- stock_base/fsm.py
+++ stock_base/fsm.py
@@ -2,7 +2,7 @@ from transitions.extensions import GraphMachine
 
 from stock_base.utils import*
 
-fsm_image_url = "https://i.imgur.com/A7LYm3K.jpg"
+fsm_image_url = "https://i.imgur.com/QiLXZJq.jpg"
 
 
 class TocMachine(GraphMachine):
@@ -72,20 +72,33 @@ class TocMachine(GraphMachine):
         print("I'm entering performance",self.current_model)
         reply_token = event.reply_token
         if self.current_model=="Tw50":
+            back_action = get_back_action('tw')
             text=TextSendMessage(text="這是在電機系一堂課之中的股票投資模擬績效平台，下圖的績效主要是因為目前這模型是以正反對作且以急快速停損的方式降低損失以最大化投資報酬率。實際上人類操作並沒辦法做到這種方式，所以我有特別針對這部分去做修改。所以這部分的績效圖看看就好。投資理財請謹慎小心!"
                                       "")
             line_bot_api.reply_message(reply_token,
-                                       [text,ImageSendMessage(original_content_url="https://i.imgur.com/v9aRaUV.jpg", preview_image_url="https://i.imgur.com/v9aRaUV.jpg")])
+                                       [text,ImageSendMessage(original_content_url="https://i.imgur.com/v9aRaUV.jpg", preview_image_url="https://i.imgur.com/v9aRaUV.jpg"),back_action])
+        else:
+            get_rl_performance(reply_token)
     def on_enter_state_intro(self,event):
         print("I'm entering intro",self.current_model)
         reply_token = event.reply_token
         if self.current_model=="Tw50":
             send_tw50_intro(reply_token)
+        else:
+            back_action = get_back_action('rl')
+            text = TextSendMessage(
+                text="這是一篇來自2020ICAIF的用RL做股票交易的期刊文章，把它應用在台股資料上的實作。論文名稱是Deep Reinforcement Learning for Automated Stock Trading: An Ensemble Strategy。\n考慮版權問題以及環境設置上會和原有環境衝突，所以這邊並不會實裝每日選股這項功能，但會在績效的地方簡單展示做出來的效果。")
+            line_bot_api.reply_message(reply_token,[text,back_action])
     def on_enter_state_stock_list(self,event):
         print("I'm entering stock_list",self.current_model)
         reply_token = event.reply_token
         if self.current_model=="Tw50":
             show_stock_list_Tw50(reply_token)
+        else:
+            back_action = get_back_action('rl')
+            text = TextSendMessage(text="to be continue")
+            line_bot_api.reply_message(reply_token, [text, back_action])
+
 
 
 
--- stock_base/utils.py
+++ stock_base/utils.py
@@ -11,9 +11,32 @@ line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 def send_text_message(reply_token, text):
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
+def get_back_action(type):
+    back_action = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            title=' ',
+            text=" ",
+            actions=[PostbackTemplateAction(
+                label='Back',
+                text='back_'+type,
+                data='A&performance'
+            )]
+        )
+    )
+    return back_action
+
 def send_image_message(reply_token,image_url):
     line_bot_api.reply_message(reply_token,ImageSendMessage(original_content_url=image_url,preview_image_url=image_url))
 
+def get_rl_performance(reply_token):
+    back_action = get_back_action('rl')
+    money_url="https://imgur.com/8bmO0jY.jpg"
+    other_url="https://imgur.com/RuI6OfS.jpg"
+    money=ImageSendMessage(original_content_url=money_url, preview_image_url=money_url)
+    other = ImageSendMessage(original_content_url=other_url, preview_image_url=other_url)
+    money_text=TextSendMessage(text="這邊就簡單比較RL模型和台灣加權指數這一年的獲利率。藍色線是RL模型橘色線是台灣加權指，可以看到其實RL訓練出來的模型走勢根台指期非常接近。在大跌的時候會跟著大跌，大漲的時候也會大漲，比較特別的就是RL似乎可以放大波動的趨勢，簡單推測就是所學的模型為了快速最大化獲利會去選波動大的股票，像大立光這樣的股票就被長期持有。")
+    line_bot_api.reply_message(reply_token,[money_text,money,other,back_action])
 def send_tempelate_message_choose_model(reply_token):
     line_bot_api.reply_message(  # 回復「選擇地區」按鈕樣板訊息
         reply_token,
@@ -78,12 +101,14 @@ def send_tempelate_message_model_info(reply_token,model_name):
         )
     )
 def send_tw50_intro(reply_token):
+    back_action = get_back_action('tw')
     article= "ML_TW50:\nML_TW50是我將所有台灣50的成分股票以機器學習的方式分別建置50個ensemble based的模型，最後再從這些模型組出實際使用的模型來做每日交易策略的決策。\nData:\n每隻模型的training data長短其實不太一樣，是根據該支股票開始時間來做設定的。"
     article_tech="\n\n技術指標:\n這個模型比較特別的就是input有140幾種技術指標加上連續多天的資訊，所以在訓練上輸入維度會多達500多維。"
     article_model="\n\n模型:\n基礎模型我是選用Ensemble 方法中的xgboost 來做使用。主要考量是想透過boosting的概念來優化這類分類任務的效能。"
     article_work="\n\n運行:\n總共50個訓練出來的模型，我會回測2018~2020的股價資訊來看這幾隻模型的勝率和實際的效能來做實際在使用模型的時候，模型組合的選用。"
     article=article+article_tech+article_model+article_work
-    send_text_message(reply_token,article)
+    line_bot_api.reply_message(reply_token, [TextSendMessage(text=article),back_action])
+
 
 def show_stock_list_Tw50(reply_token):
     now = datetime.now()
@@ -123,18 +148,7 @@ def show_stock_list_Tw50(reply_token):
         )
     )
 
-    back_action = TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-            title=' ',
-            text=" ",
-            actions=[PostbackTemplateAction(
-                label='Back',
-                text='back_tw',
-                data='A&performance'
-            )]
-        )
-    )
+    back_action =get_back_action('tw')
     line_bot_api.reply_message(reply_token, [Carousel_template, back_action
                                              ])
 
